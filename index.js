7d05ceaa7379ff1b6c58c9aa522f06b0e53ae8ed
RectangleEquals <RectangleEquals@gmail.com>
2021-06-16 07:53:54 -0700

    testing

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const server = require('./server');
 
-server.ws.on("connection", function(ws, req) {
+server.ws.on("connection", function(ws, req)
+{
+  var options = {
+    host: 'ipv4bot.whatismyipaddress.com',
+    port: 80,
+    path: '/'
+  };
+  
+  var addrExternal = "";
+
+  server.http.get(options, function(res) {
+    console.log("status: " + res.statusCode);
+    
+    res.on("data", function(chunk) {
+      addrExternal += chunk;
+    });
+  }).on('error', function(e) {
+    console.log("error: " + e.message);
+  });
+
   let addrServer = req.socket.localAddress;
   let addrRemote = req.socket.remoteAddress;
 
   var id = setInterval(function() {
     let msg = "<div>Server time: " + JSON.stringify(new Date()) + "</div><br/>";
     msg += "<div>Server address: " + addrServer + "</div>";
+    msg += "<div>External address: " + addrExternal + "</div>";
     msg += "<div>Remote address: " + addrRemote + "</div>";
     ws.send(msg, function() { })
   }, 1000)
