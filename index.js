b6e288f0d018d958c2ad52e25f9bc4692e1cca66
RectangleEquals <RectangleEquals@gmail.com>
2021-06-16 08:18:33 -0700

    testing

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
-//const config = require(__dirname + '/config/config.json');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const server = require('./server');
-var http = require("http");
 
 server.ws.on("connection", function(ws, req)
 {
-  var options = {
-    host: 'ipv4bot.whatismyipaddress.com',
-    port: 80,
-    path: '/'
-  };
-  
-  var addrExternal = "";
-
-  http.get(options, function(res) {
-    console.log("status: " + res.statusCode);
-    
-    res.on("data", function(chunk) {
-      addrExternal += chunk;
-    });
-  }).on('error', function(e) {
-    console.log("error: " + e.message);
-  });
-
-  let addrServer = req.socket.localAddress;
-  let addrRemote = req.socket.remoteAddress;
-
-  var id = setInterval(function() {
-    let msg = "<div>Server time: " + JSON.stringify(new Date()) + "</div><br/>";
-    msg += "<div>Server address: " + addrServer + "</div>";
-    msg += "<div>External address: " + addrExternal + "</div>";
-    msg += "<div>Remote address: " + addrRemote + "</div>";
-    ws.send(msg, function() { })
-  }, 1000)
-
   console.log("websocket connection open")
+
+  ws.send("OK", function() { })
+  ws.on('message', function(message) {
+    const channel = bot.channels.cache.get(process.env.CHANNEL_ID);
+    channel.send(message);
+  });
 
   ws.on("close", function() {
     console.log("websocket connection close")
@@ -49,7 +24,7 @@ bot.on('ready', function () {
 
 bot.on('message', message => {
   if (message.content === 'ping') {
-    message.reply('pong !')
+    message.reply('pong!')
   }
 })
 
